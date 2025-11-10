e2e6cf1345facfcd411a9747cb3dc4e4ce56619a
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-10 22:45:40 +0700

    operasi math

---
+++ Go6-OperasiMtk.go
@@ -0,0 +1,17 @@
+package main
+
+func opmath() {
+	var a = 2
+	var b = 3
+	var plusAb = a + b
+	var minAb = a - b
+	var crossAb = a * b
+	var divAb = a / b
+	var modAb = a % b
+
+	println(plusAb)
+	println(minAb)
+	println(crossAb)
+	println(divAb)
+	println(modAb)
+}
