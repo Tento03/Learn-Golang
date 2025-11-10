78e311b198344acdfccba52231970be2c32d590d
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-10 21:56:40 +0700

    variable

---
+++ Go3-Variable.go
@@ -0,0 +1,13 @@
+package main
+
+func variable() {
+	var name string = "Tento"
+	var age int = 21
+	var score float32 = 10.00
+	var isMc bool = true
+
+	println("Name:", name)
+	println("Age:", age)
+	println("Score Char:", score)
+	println("Status Mc:", isMc)
+}
