5f9c4f9618575d42b1786706328f35d7ca313c40
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-11-11 12:38:12 +0700

    switch case

---
+++ Go12-SwitchCase.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func switchcase() {
+	var color = "merah"
+
+	switch color {
+	case "merah":
+		fmt.Println("Red")
+	case "kuning":
+		fmt.Println("Yellow")
+	case "hijau":
+		fmt.Println("Green")
+	}
+}
